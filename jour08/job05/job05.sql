0d7ba7af56da53235f72fd2668efcf3cbc12621e
lea-martel <lea.martel@laplateforme.io>
2020-05-14 19:15:47 +0200

    jour08

---
+++ jour08/job05/job05.sql
@@ -0,0 +1,3 @@
+SELECT "prenom", "nom", "naissance" FROM etudiants WHERE id=2
+
+SELECT "prenom", "nom", "naissance" FROM etudiants WHERE sexe="femme"
---
+++ jour08/job14/job14.sql
@@ -0,0 +1,1 @@
+SELECT nom, prenom, naissance FROM etudiants WHERE naissance BETWEEN '1998-01-02' AND '2018-01-05'
